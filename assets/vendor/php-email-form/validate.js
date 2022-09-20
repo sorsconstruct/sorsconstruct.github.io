299564b4cee9b380691177b1af2e5465414fbe91
sorsconstruct <113987395+sorsconstruct@users.noreply.github.com>
2022-09-20 15:04:17 +0300

    Add files via upload

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,8 +1,7 @@
-/**
-* PHP Email Form Validation - v3.5
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
+ 
+
+/*
+
 (function () {
   "use strict";
 
@@ -79,3 +78,5 @@
   }
 
 })();
+
+*/
