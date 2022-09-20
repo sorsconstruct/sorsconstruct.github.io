bd6790acd7fe886fe5e4431c8d8b916d210c3f98
sorsconstruct <113987395+sorsconstruct@users.noreply.github.com>
2022-09-20 15:39:29 +0300

    4
    4

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,1 +1,16 @@
- 
+$('.btn-buy').submit(function () {
+    // $('#contact-form1').validate();
+
+    //if (validate()) {
+        sendFormData();
+
+        // <a href="mailto:[email protected]?subject=Testing out mailto!&body=This is only a test!">Second Example</a>
+
+       
+    //}
+    //else {
+    //    messageSendError("Warning! Please validate captcha before send new message.");
+    //}
+
+    return false;
+});
