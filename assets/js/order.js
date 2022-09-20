a5305f52f9301b1faa24114817c6ec8dbb041373
sorsconstruct <113987395+sorsconstruct@users.noreply.github.com>
2022-09-20 15:53:17 +0300

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,10 +1,16 @@
 
 $( ".btn-buy" ).on( "click", function() {
+
+
+  let countVibrationIsolator = $(this).data("id");
+  var form = document.getElementById('contact-form');
+  sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Vibration Isolator Counts: ' + countVibrationIsolator, '');
+
   // console.log( $( this ).text() );
   // $('#contact-form1').validate();
 
     //if (validate()) {
-        sendFormData();
+        // sendFormData();
 
         // <a href="mailto:[email protected]?subject=Testing out mailto!&body=This is only a test!">Second Example</a>
 
