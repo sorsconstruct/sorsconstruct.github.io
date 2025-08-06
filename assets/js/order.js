aaab4df05ce9af64217391e9432d40bc4f5573c1
pz.ibm@abv.bg <113987395+sorsconstruct@users.noreply.github.com>
2025-08-07 00:13:22 +0300

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -5,7 +5,7 @@ $(".btn-buy").on("click", function () {
   var form = document.getElementById('contact-form');
 
   if (countVibrationIsolator === 7) {
-    sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Sors Samrt Door Lock: ' + 1, '');
+    sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Sors Smart Door Lock: ' + 1, '');
   }
   else {
     sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Vibration Isolator Counts: ' + countVibrationIsolator, '');
