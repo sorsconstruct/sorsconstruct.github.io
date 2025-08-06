ca6e5718c710336d686bc7161ca4597cbcc20ea1
pz.ibm@abv.bg <113987395+sorsconstruct@users.noreply.github.com>
2025-08-07 00:10:00 +0300

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -1,28 +1,18 @@
 
-$( ".btn-buy" ).on( "click", function() {
-
+$(".btn-buy").on("click", function () {
 
   let countVibrationIsolator = $(this).data("id");
   var form = document.getElementById('contact-form');
-  sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Vibration Isolator Counts: ' + countVibrationIsolator, '');
 
-  // console.log( $( this ).text() );
-  // $('#contact-form1').validate();
-
-    //if (validate()) {
-        // sendFormData();
-
-        // <a href="mailto:[email protected]?subject=Testing out mailto!&body=This is only a test!">Second Example</a>
-
-       
-    //}
-    //else {
-    //    messageSendError("Warning! Please validate captcha before send new message.");
-    //}
-
-    return false;
+  if (countVibrationIsolator === 7) {
+    sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Sors Samrt Door Lock: ' + 1, '');
+  }
+  else {
+    sendInblueMail(form[0].value, form[1].value, form[2].value, form[3].value, 'Vibration Isolator Counts: ' + countVibrationIsolator, '');
+  }
+ 
+  return false;
 
 });
 
 
- 
